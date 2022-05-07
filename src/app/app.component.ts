ea55cc5285a89965bbac7a8e4e43741c6e98dd59
gopukinapp <gopu.k@inapp.com>
2022-05-07 20:26:57 +0530

    handle click and backspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'wordle-clone';
   keyboard = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','ENTER','Z','X','C','V','B','N','M','BACKSPACE']
-
   boxes =[
     ['','','','',''],
     ['','','','',''],
@@ -17,5 +16,50 @@ export class AppComponent {
     ['','','','',''],
     ['','','','','']
   ]
+  currentRow=0;
+  currentRowIndex=0;
+  
+  //handle click when a key is pressed
+  handleClick(key:any){
+    console.log('clicked'+key);
+    //when key is enter
+    if(key==='ENTER'){
+      console.log('clicked key is enter')
+      return
+    }
+    //when key is backspace
+    if(key ==='BACKSPACE'){
+      console.log('clicked key is backspace')
+      this.removeItemsFromRow(key,this.currentRowIndex)
+      return
+    }
+    if(this.currentRow<6&&this.currentRowIndex<5){
+      this.boxes[this.currentRow][this.currentRowIndex] =key;
+      console.log('box',this.boxes)
+      this.currentRowIndex++
 
+    }
+
+  }
+
+  //functiom called when backspace pressed
+  removeItemsFromRow(letter:any,currentRownIndex:any){
+    console.log('clicked backespace')
+    let arrayToBePushed:any
+    //makking string from array, to be removed when backspace clicked
+   let formattedString= this.boxes[this.currentRow].join('');
+   //getting the word to be removed
+   let removedString=formattedString[formattedString.length-1]
+   //replacing the word
+   formattedString=formattedString.replace(removedString,'')
+   console.log({formattedString,removedString})
+   //make string to an array and push back to the initial array
+    arrayToBePushed= formattedString.split('');
+   arrayToBePushed = [...arrayToBePushed,...Array(5-arrayToBePushed.length)]
+   this.boxes[this.currentRow] = arrayToBePushed
+   this.currentRowIndex--
+   console.log({arrayToBePushed,boxes:this.boxes,currentRownIndex})
+  
+
+  }
 }
